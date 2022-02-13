23ce186d40783e48a75bf2bbb5f0d54e2e44ca07
kim-yeonjun <lletter1263@gmail.com>
2022-02-13 17:51:10 +0900

    crud update

--- src/main/java/com/jun/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jun/book/springboot/web/PostsApiController.java
@@ -1,11 +1,14 @@
 package com.jun.book.springboot.web;
 
 import com.jun.book.springboot.service.posts.PostsService;
+import com.jun.book.springboot.web.dto.PostsListResponseDto;
 import com.jun.book.springboot.web.dto.PostsResponseDto;
 import com.jun.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jun.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,9 +27,19 @@ public class PostsApiController {
         return postsService.update(id, requestDto);
     }
 
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+    }
+
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id){
         return postsService.findById(id);
     }
 
+    @GetMapping("/api/v1/posts/list")
+    public List<PostsListResponseDto> findAll() {
+        return postsService.findAllDesc();
+    }
 }
